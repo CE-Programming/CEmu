eabf5ced4f98d57c45b783654fd2ae36848641cf
MateoConLechuga <matthewwaltzis@gmail.com>
2015-12-23 12:03:05 -0800

    jacobly: Keypad fixed?

--- core/keypad.c
+++ core/keypad.c
@@ -47,14 +47,13 @@ static uint8_t keypad_read(const uint16_t pio)
 
 /* Scan next row of keypad, if scanning is enabled */
 static void keypad_scan_event(int index) {
-    uint16_t row;
-    if (keypad.current_row >= sizeof(keypad.data) / sizeof(keypad.data[0])) {
+    if (keypad.current_row >= 16) {
         return; /* too many keypad rows */
     }
 
-    row = ~keypad.key_map[keypad.current_row];
+    uint16_t row = ~keypad.key_map[keypad.current_row];
     row &= ~(0x80000 >> keypad.current_row); /* Emulate weird diagonal glitch */
-    row |= 0xFFFF << (keypad.size >> 8 & 0xFF);  /* Unused columns read as 1 */
+    row |= -1 << (keypad.size >> 8 & 0xFF);  /* Unused columns read as 1 */
     row = ~row;
 
     if (keypad.data[keypad.current_row] != row) {
@@ -87,7 +86,7 @@ static void keypad_write(const uint16_t pio, const uint8_t byte)
         case 0x00: case 0x01: case 0x02: case 0x03:
         write8(keypad.cntrl,bit_offset,byte);
         if (keypad.cntrl & 2) {
-            event_set(SCHED_KEYPAD, ((keypad.cntrl >> 16) + (keypad.cntrl >> 2 & 0x3FFF))/100);
+            event_set(SCHED_KEYPAD, ((keypad.cntrl >> 16) + (keypad.cntrl >> 2 & 0x3FFF))/1000);
         } else {
             event_clear(SCHED_KEYPAD);
         }
