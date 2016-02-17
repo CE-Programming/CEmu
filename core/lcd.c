a4465a6f127303163b2c442f511ec4602e9b6b07
Runer112 <runer112@gmail.com>
2016-02-17 11:19:43 -0500

    Corrected blank LCD logic

--- core/lcd.c
+++ core/lcd.c
@@ -71,8 +71,9 @@ void lcd_drawframe(uint32_t *out) {
     uint_fast32_t words = 320 * 240;
     uint_fast32_t word, color;
     uint32_t *in = (uint32_t *) (mem.ram.block + ((uint32_t) lcd.upcurr & (lcd_dma_size - 1)));
-    if(!lcd.upcurr || !in) {
-        memset(out, 0, 320 * 240 * 2);
+
+    if(!mem.ram.block) {
+        memset(out, 0, 320 * 240 * 4);
         return;
     }
 
