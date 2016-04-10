6d3f238c127ffb734543efc4d886f04a177e33e1
Jacob Young <jacobly0@users.noreply.github.com>
2016-04-10 05:42:44 -0400

    Some random guesses.

--- core/control.c
+++ core/control.c
@@ -6,6 +6,7 @@
 #include "lcd.h"
 #include "bus.h"
 #include "debug/debug.h"
+#include "usb/usb.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -40,11 +41,12 @@ static uint8_t control_read(const uint16_t pio, bool peek) {
             break;
         case 0x0F:
             value = control.ports[index];
-            if (control.usbBusPowered)    { value |= 0x80; }
-            if (control.usbSelfPowered) { value |= 0x40; }
-            break;
-        case 0x1C:
-            value = 0x80;
+            if (usb.regs.hcor.data[8] & 1) {
+                value |= 0x80;
+            }
+            if (usb.regs.otgcsr & 0x80000) {
+                value |= 0x40;
+            }
             break;
         case 0x1D: case 0x1E: case 0x1F:
             value = read8(control.privileged, (index - 0x1D) << 3);
