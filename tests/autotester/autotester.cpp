9d8e5e2666856731ebe38ec268e4a3635bfd2b1b
Adrien Bertrand <bertrand.adrien@gmail.com>
2016-04-25 04:23:48 -0400

    [Autotester] Added the CRC32-C lib, json11 lib, and JSON stuff
    Overall steps for the autotester implementation are in the .cpp for now.

---
+++ tests/autotester/autotester.cpp
@@ -0,0 +1,12 @@
+/* not yet done */
+
+/*
+Steps:
+- get json test config from argv
+- if ok, parse it
+- if ok, check presence of required files
+- if ok, launch cemucore with the rom specified in the json
+- if ok, follow the setup sequence specified in the json
+- when done, compute the CRCs as specified in the json and compare them with their expected value(s).
+- return 0 if all OK, or -1 if at least one failed
+*/
