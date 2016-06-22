2dfc4eac33fda424f6977b05e0bb4850a5d6aa54
Adrien Bertrand <bertrand.adrien@gmail.com>
2016-06-22 14:48:38 +0200

    Expose sendVariableLink to emscripten

--- core/link.h
+++ core/link.h
@@ -1,6 +1,12 @@
 #ifndef LINK_H
 #define LINK_H
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#else
+#define EMSCRIPTEN_KEEPALIVE
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -14,7 +20,7 @@ extern volatile bool emu_is_receiving;
 
 void enterVariableLink(void);
 bool listVariablesLink(void);
-bool sendVariableLink(const char *var_name);
+bool EMSCRIPTEN_KEEPALIVE sendVariableLink(const char *var_name);
 bool receiveVariableLink(int count, const calc_var_t *vars, const char *file_name);
 
 #ifdef __cplusplus
