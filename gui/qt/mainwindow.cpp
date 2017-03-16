ce49295dd1ce6cb49d790e2524d207ec04a54082
MattWaltz <matthewwaltzis@gmail.com>
2017-03-16 11:15:04 -0700

    Stop blocking call to exec which prevents close

--- gui/qt/mainwindow.cpp
+++ gui/qt/mainwindow.cpp
@@ -1012,11 +1012,12 @@ void MainWindow::variableClicked(QTableWidgetItem *item) {
         updateDisasmView(var_tmp.address + 4, false);  // This is
         if (!inDebugger) { debuggerChangeState(); }    // semi-broken
     } else if (!calc_var_is_internal(&var_tmp) || var_tmp.name[0] == '#') {
-        BasicCodeViewerWindow codePopup(this);
-        codePopup.setOriginalCode((var_tmp.size <= 500) ? ui->emuVarView->item(item->row(), 3)->text() : QString::fromStdString(calc_var_content_string(var_tmp)));
-        codePopup.setVariableName(ui->emuVarView->item(item->row(), 0)->text());
-        codePopup.show();
-        codePopup.exec();
+        BasicCodeViewerWindow *codePopup = new BasicCodeViewerWindow(this);
+        codePopup->setOriginalCode((var_tmp.size <= 500) ? ui->emuVarView->item(item->row(), 3)->text() : QString::fromStdString(calc_var_content_string(var_tmp)));
+        codePopup->setVariableName(ui->emuVarView->item(item->row(), 0)->text());
+        codePopup->setWindowModality(Qt::NonModal);
+        codePopup->setAttribute(Qt::WA_DeleteOnClose);
+        codePopup->show();
     }
 }
 
