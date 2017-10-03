96fd4719c7714d63594ec241b0498b8f79251e82
Albert Huang <alberthdev@users.noreply.github.com>
2017-10-02 20:50:33 -0400

    Website initial commit

---
+++ deploy_travisci.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Automated deploy script with Travis-CI.
+
+cerr() {
+    if [ ! "$1" = "0" ]; then
+        echo "ERROR: Previous command exited with code $1."
+        echo "       See above for details."
+        echo "       Command: ${@:2}"
+        exit $1
+    fi
+}
+
+# Variables
+echo "Started deployment..."
+
+# Build site.
+echo " -> Building website..."
+pip install -r requirements.txt; cerr $? "Python Pip Requirements Installation"
+python3 generate.py; cerr $? "Webpage Generation"
+
+cp -r css fonts img js html/; cerr $? "Copying Webpage Support Files"
+
+#echo " -> Notifying IRC..."
+
+echo "Website built successfully!"
+exit 0
---
+++ generate.py
@@ -0,0 +1,22 @@
+import jinja2
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+templateLoader = jinja2.FileSystemLoader( searchpath=os.path.join(base_dir, "templates"))
+templateEnv = jinja2.Environment(loader=templateLoader, lstrip_blocks=True, trim_blocks=True)
+
+def generate_file(file_name):
+	file_name = file_name + ".html"
+
+	template = templateEnv.get_template( file_name )
+	outputText = template.render() # this is where to put args to the template renderer
+
+	file = open(os.path.join(base_dir, "html", file_name), 'w')
+	file.write(outputText)
+
+	print("Rendered", file_name)
+
+files = ['404', 'index', 'download']
+for file in files:
+	generate_file(file)
