858f47c7c60550573506af766b64944b3459f56b
Jacob Young <jacobly0@users.noreply.github.com>
2017-12-05 14:53:56 -0500

    Update apng install script.

--- gui/qt/capture/get_libpng-apng.sh
+++ gui/qt/capture/get_libpng-apng.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-LIBPNG_VERSION=1.6.34
+LIBPNG_VERSION=$(wget -q -O- 'https://sourceforge.net/p/libpng/code/ref/master/tags/' |
+                     sed -n 's,.*<a[^>]*>v\([0-9][^<]*\)<.*,\1,p' |
+                     grep -v alpha | grep -v beta | grep -v rc | sort -V | tail -1)
 
 # Download libpng
-wget "https://downloads.sourceforge.net/sourceforge/libpng/libpng-${LIBPNG_VERSION}.tar.xz"
+wget -q "https://downloads.sourceforge.net/sourceforge/libpng/libpng-${LIBPNG_VERSION}.tar.xz"
 tar -xJf libpng-${LIBPNG_VERSION}.tar.xz
 rm -f libpng-${LIBPNG_VERSION}.tar.xz
 
 # Download apng patch
-wget "https://downloads.sourceforge.net/sourceforge/libpng-apng/libpng-${LIBPNG_VERSION}-apng.patch.gz"
+wget -q "https://downloads.sourceforge.net/sourceforge/libpng-apng/libpng-${LIBPNG_VERSION}-apng.patch.gz"
 gzip -d libpng-${LIBPNG_VERSION}-apng.patch.gz
 
 # Apply apng patch
@@ -16,16 +18,9 @@ cd libpng-${LIBPNG_VERSION}
 patch -Np1 -i ../libpng-${LIBPNG_VERSION}-apng.patch
 rm -f ../libpng-${LIBPNG_VERSION}-apng.patch
 
-# Change libname to avoid conflicts
-sed -i.bak -e "s/LIBNAME\=\'PNG\@PNGLIB/LIBNAME\=\'APNG\@PNGLIB/" Makefile.am
-rm -f Makefile.am.bak
-
 # Build libpng-apng
 CFLAGS="-O2 -fPIC" ./configure --enable-static --disable-shared
 make -j2
-mv .libs/libpng*.a .libs/libpngapng.a
+sudo make install
 
 cd ..
-
-# CEmu will expect this name
-mv libpng-${LIBPNG_VERSION} libpng-apng
