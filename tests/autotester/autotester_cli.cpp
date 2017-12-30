a74aeb76431f0077ef6b319fb985ce1c3c6dc31d
Adrien Bertrand <bertrand.adrien@gmail.com>
2017-12-30 18:23:06 +0100

    Fix missing autotester cstdarg include

--- tests/autotester/autotester_cli.cpp
+++ tests/autotester/autotester_cli.cpp
@@ -14,6 +14,7 @@
 #include <thread>
 #include <chrono>
 #include <regex>
+#include <cstdarg>
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
   #include <direct.h>
