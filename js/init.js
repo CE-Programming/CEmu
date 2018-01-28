67fc7424eb1691aeabb87251edb9fa0d0bd288c0
Albert Huang <alberthdev@users.noreply.github.com>
2018-01-28 17:08:10 -0500

    Partially address #162 for the v1.0 download page...
     * Fixed overlay on mobile menu. Materialize isn't clear on navbar
       mobile location, but some bug reading shows that it should be moved
       out of the div and place it as its own entity. See:
       https://github.com/Dogfalo/materialize/issues/2879
    
     * Fixed mobile menu width. See:
       https://github.com/Dogfalo/materialize/issues/2102
    
     * Merge downloads pages and instead add a sub-tab for stable
       releases. (This required some quirky workarounds for tab
       behavior... see: https://github.com/Dogfalo/materialize/issues/2102)
    
     * Subtab support also forces a special CSS class to workaround
       issues in Materialize. (Talk about buggy...)
       See: https://github.com/Dogfalo/materialize/issues/4029

--- js/init.js
+++ js/init.js
@@ -1,7 +1,33 @@
 (function($){
   $(function(){
 
+    // One time tab init
+    var oneTimeTabInit = false;
+
     $('.button-collapse').sideNav();
+
+    // https://github.com/Dogfalo/materialize/issues/2102#issuecomment-284171287
+    $('.collapsible').collapsible({
+      onOpen: function(el) {
+        // https://github.com/Dogfalo/materialize/issues/2102
+        window.dispatchEvent(new Event('resize'));
+      }
+    });
+  
+    // Similarly, for subtabs...
+    // We only init once because otherwise the animation doesn't fire
+    // anymore when resizes occur.
+    $('ul.tabs').tabs({
+      onShow: function(el) {
+        // https://github.com/Dogfalo/materialize/issues/2102
+        console.log("Tab change detected");
+        if (!oneTimeTabInit) {
+          window.dispatchEvent(new Event('resize'));
+          oneTimeTabInit = true;
+        }
+        return true;
+      }
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
