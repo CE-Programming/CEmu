c6a2331e2ee42bac40efb0ab079b3aef400df622
Jacob Young <jacobly0@users.noreply.github.com>
2019-08-21 09:47:12 -0400

    I guess we will need an ARM emulator or something.

---
+++ core/arm/armcpu.h
@@ -0,0 +1,37 @@
+#ifndef ARMCPU_H
+#define ARMCPU_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum arm_exception_number {
+    ARM_Exception_Reset = 1,
+    ARM_Exception_NMI = 2,
+    ARM_Exception_HardFault = 3,
+    ARM_Exception_SVCall = 11,
+    ARM_Exception_PendSV = 14,
+    ARM_Exception_SysTick = 15,
+} arm_exception_number_t;
+
+typedef union arm_cpu_state {
+    uint32_t r[32];
+    struct {
+        uint32_t r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, sp, lr, pc, altsp;
+        uint8_t excNum : 6;
+        bool v, c, z, n, pm, spsel, mode;
+    };
+} arm_cpu_state_t;
+
+extern arm_cpu_state_t arm_cpu;
+
+void arm_execute(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- gui/qt/CEmu.pro
+++ gui/qt/CEmu.pro
@@ -206,6 +206,8 @@ if(macx) {
 
 SOURCES += \
     ../../tests/autotester/autotester.cpp \
+    ../../core/arm/armcpu.c \
+    ../../core/arm/armmem.c \
     ../../core/asic.c \
     ../../core/bootver.c \
     ../../core/cpu.c \
@@ -292,6 +294,8 @@ SOURCES +=  ../../tests/autotester/autotester_cli.cpp \
 
 HEADERS  += \
     ../../tests/autotester/autotester.h \
+    ../../core/arm/armcpu.h \
+    ../../core/arm/armmem.h \
     ../../core/asic.h \
     ../../core/bootver.h \
     ../../core/cpu.h \
