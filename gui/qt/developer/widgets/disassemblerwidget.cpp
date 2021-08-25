9b439bd9f94202702c4eea87716dadd54639dcd6
Jacob Young <jacobly0@users.noreply.github.com>
2021-08-25 04:28:19 -0400

    Update copyright years with sed.

--- gui/qt/developer/widgets/disassemblerwidget.cpp
+++ gui/qt/developer/widgets/disassemblerwidget.cpp
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2015-2020 CE Programming.
+ * Copyright (c) 2015-2021 CE Programming.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
