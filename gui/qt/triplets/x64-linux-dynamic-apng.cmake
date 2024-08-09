d934c67dd8d6a78d62240e5da3d0ced125186524
Brendan Fletcher <calc84maniac@gmail.com>
2024-08-09 14:44:29 -0400

    linux: ci: Use static libpng-apng for real

--- gui/qt/triplets/x64-linux-dynamic-apng.cmake
+++ gui/qt/triplets/x64-linux-dynamic-apng.cmake
@@ -1,7 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
-if(PORT STREQUAL "png")
+if(PORT STREQUAL "libpng")
     set(VCPKG_LIBRARY_LINKAGE static)
 endif()
 
